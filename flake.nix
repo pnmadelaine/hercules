8e04f54441726fa2cded19e51fa6a87c0494b594
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-22 17:33:56 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.${system}.default = pkgs.hello;
+  };
+}
